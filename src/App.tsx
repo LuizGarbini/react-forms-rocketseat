44f5f63cea5a8c36845ce226f2dabf7c82ac7399
LuizGarbini <garbasneto@gmail.com>
2025-05-20 18:09:18 -0300

    onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
-import "./App.css"
+import "./App.css";
+
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div>
-      <h1>Evento</h1>
+	const [name, setName] = useState("");
 
-      <form>
-        <input type="text" placeholder="Nome do evento" />
-        <span className="error">Nome é obrigatório</span>
+	return (
+		<div>
+			<h1>Evento {name}</h1>
 
-        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+			<form>
+				<input
+					type="text"
+					placeholder="Nome do evento"
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<span className="error">Nome é obrigatório</span>
 
-        <select defaultValue="">
-          <option value="" disabled>
-            Selecione...
-          </option>
+				<input type="date" placeholder="Nome do evento" lang="pt-BR" />
 
-          <option value="technology">React</option>
-          <option value="entertainment">Node.js</option>
-          <option value="business">Javascript</option>
-          <option value="business">Typescript</option>
-        </select>
+				<select defaultValue="">
+					<option value="" disabled>
+						Selecione...
+					</option>
 
-        <textarea placeholder="Descrição" rows={4} />
+					<option value="technology">React</option>
+					<option value="entertainment">Node.js</option>
+					<option value="business">Javascript</option>
+					<option value="business">Typescript</option>
+				</select>
 
-        <button type="submit">Salvar</button>
-      </form>
-    </div>
-  )
+				<textarea placeholder="Descrição" rows={4} />
+
+				<button type="submit">Salvar</button>
+			</form>
+		</div>
+	);
 }
