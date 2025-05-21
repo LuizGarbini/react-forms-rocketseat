3810397fc8f4e090339fba763acaaa77630c4ecd
LuizGarbini <garbasneto@gmail.com>
2025-05-21 13:50:36 -0300

    Utilizando Schema na Validação

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,15 @@ type FormData = {
 	description: string;
 };
 
-const schema = yup.object();
+const schema = yup.object({
+	name: yup.string().required("Nome é obrigatório"),
+	date: yup.string().required("Data é obrigatória"),
+	subject: yup.string().required("Selecione um assunto"),
+	description: yup
+		.string()
+		.required("Descrição é obrigatória")
+		.min(10, "A descrição precisa ter pelo menos 10 digitos"),
+});
 
 export default function App() {
 	const { control, handleSubmit } = useForm<FormData>({
